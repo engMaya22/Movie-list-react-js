02a10cc9b877e5daac246b058e45e772296f1864
engMaya22 <eng.maya.esmaeel1@gmail.com>
2025-02-23 17:39:14 +0300

    home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+
+import MovieCard from "../componenets/MovieCard";
+import { useState } from "react";
+
+
+
+const Home = ()=>{
+    const [searchQuery , setSearchQuery] = useState('');//whenever state update , the component will rerender
+
+
+    const movies = [
+        {id:1, title:'first' , release_date : '2025'},  
+        {id:2, title:'second' , release_date : '2026'},  
+        {id:3, title:'three' , release_date : '2027'}
+    ];
+
+    const handleSearch = (e)=>{
+          e.preventDefault();
+          alert(searchQuery);
+          setSearchQuery("")
+
+    }
+
+    return <div className="home">
+              <form onSubmit={handleSearch} className="search-form">
+                   <input  type="text"
+                         placeholder="Seach for movies ..."  
+                         className="search-input"
+                         value={searchQuery}
+                         onChange={(e)=>setSearchQuery(e.target.value)}
+                       
+                         />
+                         
+                    <button type="submit" className="search-button">
+                      Search
+                    </button>
+
+              </form>
+
+               <div className="movies-grid" >
+                {
+                    movies.map((movie , id)=> 
+                         <MovieCard movie={movie} key={id}/>
+                    )
+                }
+               </div>
+
+          </div>
+}
+export default Home;
