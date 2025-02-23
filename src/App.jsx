90f763a9238fd588d827ba8e727eaa8599a2c65e
engMaya22 <eng.maya.esmaeel1@gmail.com>
2025-02-23 18:10:10 +0300

    Navbar && favourites pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Favourites from './pages/Favourites'
+import NavBar from './componenets/NavBar'
 const App = ()=>{
   return (
-    <>
-      < Home />
-
-   
-    </>
+       <div>
+         <NavBar />
+         <main className='main-content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/favourites' element={<Favourites />} />
+          </Routes>
+         </main>
+       </div>
   )
 }
 
---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,11 @@
+
+
+const Favourites = ()=>{
+    return <div className="favourites-empty">
+                <h2>No Favouite Movies Yet</h2>
+                <p>Start adding movies to your favourites , they will appear here .</p>
+
+    </div>
+
+}
+export default Favourites ;
